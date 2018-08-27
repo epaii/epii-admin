2f3171ec12356762a84372084b37dc2e177c74b3
MrRen <543169072@qq.com>
2018-08-27 09:02:50 +0800

    init

--- public/epiiadmin-js/js/model/epii.suggest.js
+++ public/epiiadmin-js/js/model/epii.suggest.js
@@ -4,15 +4,15 @@
 (function (factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as anonymous module.
-        define(['jquery','bootstrap-suggest'], factory);
+        define(['jquery', 'bootstrap-suggest'], factory);
     } else if (typeof exports === 'object') {
         // Node / CommonJS
-        factory(require('jquery'),require('bootstrap-suggest'));
+        factory(require('jquery'), require('bootstrap-suggest'));
     } else {
         // Browser globals.
         factory(jQuery);
     }
-})(function ($,bootstrapSuggest) {
+})(function ($, bootstrapSuggest) {
 
     var out = {
         init: function (selects) {
@@ -25,35 +25,51 @@
                     separator: ",",          //多关键字支持时的分隔符，默认为空格
                     getDataMethod: "url",    //获取数据的方式，总是从 URL 获取
                     url: data['url'], /*优先从url ajax 请求 json 帮助数据，注意最后一个参数为关键字请求参数*/
-                    listAlign:"auto",
-                    idField: EpiiAdmin.getTrueValue(data['id-field'],"id"),                    //每组数据的哪个字段作为 data-id，优先级高于 indexId 设置（推荐）
+                    listAlign: "auto",
+                    idField: EpiiAdmin.getTrueValue(data['id-field'], "id"),                    //每组数据的哪个字段作为 data-id，优先级高于 indexId 设置（推荐）
 
-                    keyField: EpiiAdmin.getTrueValue(data['key-field'],"text"),
+                    keyField: EpiiAdmin.getTrueValue(data['key-field'], "text"),
 
                     // url 获取数据时，对数据的处理，作为 fnGetData 的回调函数
                     fnProcessData: function (json) {
                         return {value: json};
                     }
                 }).on('onDataRequestSuccess', function (e, result) {
-                   // console.log('onDataRequestSuccess: ', result);
+                    // console.log('onDataRequestSuccess: ', result);
                 }).on('onSetSelectValue', function (e, keyword, data) {
-                    console.log('onSetSelectValue: ', keyword, data);
+                   // console.log('onSetSelectValue: ', keyword, data);
 
 
-                    for(var item  in data)
-                    {
+                    $(that).attr("data-last-select-value", e.target.value);
+                    var inputs = [];
+                    for (var item  in data) {
 
-                        var input ="input"+ item.substring(0,1).toUpperCase()+item.substring(1);
-                        console.log(input);
+                        var input = "input" + item.substring(0, 1).toUpperCase() + item.substring(1);
+
                         if ($(that).data(input)) {
                             $("input[name='" + $(that).data(input) + "']").val(data[item]);
+                            inputs.push($(that).data(input));
                         }
                     }
+                    $(that).attr("data-inputs", inputs.join(","));
 
+                    var callback = EpiiAdmin.tools.getFunction(null, $(that).data(), "onSelect", window);
+
+                    callback.apply(this, arguments);
                 }).on('onUnsetSelectValue', function () {
-                    console.log("onUnsetSelectValue");
+                    //console.log("onUnsetSelectValue");
                 }).on('onHideDropdown', function (e, data) {
-                    console.log('onHideDropdown', e.target.value, data);
+                   // console.log('onHideDropdown', e.target.value, data);
+                    if ($(that).attr("data-last-select-value") != e.target.value) {
+                        var inputs = $(that).attr("data-inputs").split(",");
+                        for (var item  in inputs) {
+
+
+                            $("input[name='" + inputs[item] + "']").val("");
+
+
+                        }
+                    }
 
                 });
             });
